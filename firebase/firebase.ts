9749de3e14c29f93dde4b75f17b479ab5bbc6d89
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-21 18:52:54 -0300

    Agregando config

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,2 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+const firebaseConfig = {
+    apiKey: "AIzaSyAmobyJ0-r1o4H0zGMDeL6GtuS4gmxsir4",
+    authDomain: "pleh-20a48.firebaseapp.com",
+    projectId: "pleh-20a48",
+    storageBucket: "pleh-20a48.appspot.com",
+    messagingSenderId: "215017658521",
+    appId: "1:215017658521:web:b456ff32cfd8381a0cc008",
+    measurementId: "G-GMVJSBPJG8"
+  };
