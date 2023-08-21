c2798b01cbbd515e9e7ae1cbae61447c5fe68f0d
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-21 17:44:00 -0300

    Importando analiticas de firebase

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Login from './assets/src/login/login';
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 export default function App() {
   return (
     <React.Fragment>
