c89ab239f578de6dca0411e601e28296878c4be8
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-21 17:43:35 -0300

    Importando inciador de app

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Login from './assets/src/login/login';
+import { initializeApp } from "firebase/app";
 export default function App() {
   return (
     <React.Fragment>
