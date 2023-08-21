8eccb7f9fea0abe38e37040a2df7bfce35da6d1e
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-21 18:06:59 -0300

    Preventdefault

--- assets/src/login/login.jsx
+++ assets/src/login/login.jsx
@@ -6,6 +6,7 @@ function Login(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const handleLogin = async (e) => {
+        e.preventDefault();
     }
     return(
         <form>
