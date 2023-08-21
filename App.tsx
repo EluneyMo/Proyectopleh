9eac37749d0cb82aceefd6af71d5c4e854bd4a4a
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-21 17:05:12 -0300

    Importando componente login

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Login from './assets/src/login/login';
 export default function App() {
   return (
     <View style={styles.container}>
