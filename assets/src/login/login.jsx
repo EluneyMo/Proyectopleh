a5fb86af373dfacfb296b8d225bcf2543cc21f2f
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-21 16:41:46 -0300

    Agregando archivos perdidos

---
+++ assets/src/login/login.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function Login(){
+    return(
+        <form>
+            <div class="mb-3">
+                <label for="Email" class="form-label">Email</label>
+                <input type="deliveremail" class="form-control" id="Email" aria-describedby="emailHelp"/>
+                <div id="emailHelp" class="form-text">Tu informacion no sera compartida con nadie.</div>
+            </div>
+            <div class="mb-3">
+                <label for="contra" class="form-label">Contraseña</label>
+                <input type="contraseña" class="form-control" id="contra"/>
+            </div>
+            <div class="mb-3-check">
+                <input type="checkbox" class="form-check-input" id="check"/>
+                <label class="checklabel" for="check">Recuerdame</label>
+            </div>
+            <button type="submit" class="btn">Enviar</button>
+        </form>
+    )
+
+}
+export default Login;
