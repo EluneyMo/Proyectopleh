47b335551866a553ca12897e0029ac9cc4d91e57
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-23 10:01:50 -0300

    Agregando interfaz

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import Login from './assets/src/login/login';
 import Firebase from './firebase/firebase';
-
+interface AuthenticatedUser{
+  
+}
 export default function App() {
   const [user, setUser] =useState<Firebase.User | null>(null);
   useEffect(()=>  {
