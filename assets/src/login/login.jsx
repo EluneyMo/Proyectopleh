439b2d84187077676867076b964d1d35454d6be6
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-23 11:27:27 -0300

    Eliminando

--- assets/src/login/login.jsx
+++ assets/src/login/login.jsx
@@ -16,7 +16,7 @@ function Login() {
     }
     try {
       // Usar el servicio de autenticación de Firebase para iniciar sesión con email y contraseña
-      await Firebase.auth().signInWithEmailAndPassword(email, password);
+      await auth.signInWithEmailAndPassword(email, password);
       console.log("Usuario autenticado");
       if (auth.currentUser){
         const userRef = database.ref(`users/${auth.currentUser.uid}`);
