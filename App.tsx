da9a7959b2582a1588509e0245058794d3d46f8e
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-23 10:03:41 -0300

    Definiendo interfaz de usuario autenticado

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import React, {useState, useEffect} from 'react';
 import Login from './assets/src/login/login';
 import Firebase from './firebase/firebase';
 interface AuthenticatedUser{
-  
+  uid:string;
+  email:string|null;
 }
 export default function App() {
   const [user, setUser] =useState<Firebase.User | null>(null);
