446d1bb0593e6dcdab3c230925d98f21f7ec6f2a
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-23 09:32:54 -0300

    Exportando firebase

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Login from './assets/src/login/login';
-import {app, auth} from './firebase/firebase'
+import firebase from './firebase/firebase'
 
 export default function App() {
   return (
