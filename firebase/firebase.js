37def3b08a91c4983ce4162a9d82573148b5705a
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-23 11:54:12 -0300

    Eliminando

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,7 @@
-import { firebase } from '@react-native-firebase/auth';
-import {initializeApp} from '../node_modules/firebase/app';
-import  {getAuth}  from '../node_modules/firebase/auth';
-import {getDatabase} from '../node_modules/firebase/database';
+
+import firebase from '../node_modules/firebase/app';
+import '../node_modules/firebase/auth';
+import '../node_modules/firebase/database';
 const firebaseConfig = {
     apiKey: "AIzaSyAmobyJ0-r1o4H0zGMDeL6GtuS4gmxsir4",
     authDomain: "pleh-20a48.firebaseapp.com",
