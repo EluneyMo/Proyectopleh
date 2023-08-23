de9a8585790fab91bc523c780402c92cbfe9bcd9
EluneyMo <morales.eluney.epet20@gmail.com>
2023-08-23 09:37:04 -0300

    Agregando base de datos

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,6 +5,7 @@ import '../node_modules/firebase/database';
 const firebaseConfig = {
     apiKey: "AIzaSyAmobyJ0-r1o4H0zGMDeL6GtuS4gmxsir4",
     authDomain: "pleh-20a48.firebaseapp.com",
+    databaseURL: "https://pleh-20a48-default-rtdb.firebaseio.com/",
     projectId: "pleh-20a48",
     storageBucket: "pleh-20a48.appspot.com",
     messagingSenderId: "215017658521",
