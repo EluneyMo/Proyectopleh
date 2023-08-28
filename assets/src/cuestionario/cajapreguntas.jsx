df722a855190fa8242b42261082f3206a3241b5e
nata <mercadonatali.epet20@gmail.com>
2023-08-28 15:34:28 -0300

    Creando titulo a card

--- assets/src/cuestionario/cajapreguntas.jsx
+++ assets/src/cuestionario/cajapreguntas.jsx
@@ -5,7 +5,7 @@ function Card(){
         <div className='card' style='width:18rem;'>
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
-                
+                <h5 className="card-title">Violencia de Genero</h5>
             </div>
 
         </div>
