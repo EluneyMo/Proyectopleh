cf6ef62c9ea6422015fe6291abe6d633260c1c8a
nata <mercadonatali.epet20@gmail.com>
2023-08-28 15:30:49 -0300

    Insertando imagen

--- assets/src/cuestionario/cajapreguntas.jsx
+++ assets/src/cuestionario/cajapreguntas.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Card(){
     return(
         <div className='card' style='width:18rem;'>
+            <img src="..." className="card-img-top" alt="..." />
 
         </div>
     )    
