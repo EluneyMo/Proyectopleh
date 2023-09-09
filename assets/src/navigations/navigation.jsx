d0a5974b1da84095ce4dd728f001b34daab734f9
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-09 10:45:43 -0300

    Importando dependencias

--- assets/src/navigations/navigation.jsx
+++ assets/src/navigations/navigation.jsx
@@ -1,2 +1,3 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, Link } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack'
