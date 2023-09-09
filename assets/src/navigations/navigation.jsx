bd077dabae7cfb947bea61f3fd9c39d8564845f4
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-09 10:48:53 -0300

    Importando registro

--- assets/src/navigations/navigation.jsx
+++ assets/src/navigations/navigation.jsx
@@ -2,3 +2,4 @@ import React from "react";
 import { NavigationContainer, Link } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack'
 import Login from "../login/login";
+import Registro from "../registro/register";
