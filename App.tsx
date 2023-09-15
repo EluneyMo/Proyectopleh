634a7fb4eafe4945ab26f5f32ad236018dd5b998
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-15 19:51:11 -0300

    Cambiando null a undefined

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';// Importa las funciones y la instancia de Firebase
 import Login from './assets/src/login/login';
 import Home from './assets/src/home/Home';
-
+import 
 import appFirebase from './firebase/firebase';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 const auth=getAuth(appFirebase)
 export default function App() {
-  const [usuario, setUsuario] = useState();
+  const [usuario, setUsuario] = useState<User | undefined>();
   onAuthStateChanged(auth,(usuarioFirebase)=>{
     if (usuarioFirebase){
       setUsuario(usuarioFirebase)
@@ -17,30 +17,10 @@ export default function App() {
     }
   });
   
-  // Función para iniciar sesión
-  const handleLogin = async () => {
-    try {
-      const user = await signInUserWithEmailAndPassword('correo@example.com', 'contraseña');
-      setUser(user);
-    } catch (error) {
-      console.error("Error al iniciar sesión:", error);
-    }
-  };
-
-  useEffect(() => {
-    // Escuchar cambios en la base de datos cuando el usuario esté autenticado
-    if (user) {
-      listenToDatabaseChanges('usuarios/', (data) => {
-        setDataFromDatabase(data);
-      });
-    }
-  }, [user]);
-
-
   return (
     <>
     <div>
-      
+      {usuario ? <Home correoUsuario={usuario.email}/>: <Login/>}
     </div>
     </>
   );
