9bdbce924252539f83367e1787bfd93724f1f35a
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-15 20:29:09 -0300

    Agregando text

--- assets/src/home/Home.jsx
+++ assets/src/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = ({correoUsuario}) => {
   return (
     <div>
-      Bienvenido {correoUsuario}
+      <Text>Bienvenido {correoUsuario}</Text>
     </div>
   )
 }
