c74a9ca6fe2be6fedb3c5ec0ee61477bfc226703
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-16 10:11:23 -0300

    Importando estilos

--- assets/src/home/Home.jsx
+++ assets/src/home/Home.jsx
@@ -3,6 +3,7 @@ import appFirebase from '../../../firebase/firebase'
 import { getAuth, signOut } from 'firebase/auth'
 import { Link } from "react-router-dom"
 import Button from "@material-ui/core/Button"
+import "../home/home.css"
 const auth=getAuth(appFirebase)
 const Home = ({correoUsuario}) => {
   return (
