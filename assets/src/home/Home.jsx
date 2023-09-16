cb2dba687b912e6b60c33a9d54568a7c08c95429
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-16 11:33:01 -0300

    Arreglando constante

--- assets/src/home/Home.jsx
+++ assets/src/home/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { ReactDOM } from 'react'
 import appFirebase from '../../../firebase/firebase'
 import { getAuth, signOut } from 'firebase/auth'
-import { Link, BrowserRouter as Router } from "react-router-dom"
+import { Link, redirect, BrowserRouter as Router } from "react-router-dom"
 import Button from "@material-ui/core/Button"
 import "../home/home.css"
 import Panicbutton from '../panico/Panicbutton'
 const auth=getAuth(appFirebase)
 const Home = ({correoUsuario}) => {
-  
+
+  const [redirigir, setRedirigir] = useState(false);
+  const manejarClick = () => {
+  };
   return (
     <div>
       <h1>Bienvenido {correoUsuario}  <button className='btn btn-primary' onClick={()=>signOut(auth)}>Cerrar sesion</button></h1>
