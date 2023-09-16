408c25f7b0705564a1ccc55953ed419886b2200b
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-16 11:06:42 -0300

    Creando constante

--- assets/src/home/Home.jsx
+++ assets/src/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ReactDOM } from 'react'
 import appFirebase from '../../../firebase/firebase'
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import { getAuth, signOut } from 'firebase/auth'
 import { Link, BrowserRouter as Router } from "react-router-dom"
 import Button from "@material-ui/core/Button"
@@ -9,6 +9,7 @@ import "../home/home.css"
 import Panicbutton from '../panico/Panicbutton'
 const auth=getAuth(appFirebase)
 const Home = ({correoUsuario}) => {
+  const history = useHistory();
   return (
     <div>
       <h1>Bienvenido {correoUsuario}  <button className='btn btn-primary' onClick={()=>signOut(auth)}>Cerrar sesion</button></h1>
