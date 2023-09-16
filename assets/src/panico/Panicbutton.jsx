d0d1324c4562042d7b68e900893810aa5324fd7f
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-16 10:13:11 -0300

    Cambiando nombres

--- assets/src/panico/Panicbutton.jsx
+++ assets/src/panico/Panicbutton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Accesodir = () => {
+const Panic = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Accesodir = () => {
   )
 }
 
-export default Accesodir
+export default Panic
