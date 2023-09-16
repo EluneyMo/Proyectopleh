7e2698b498a0afd88f89135a466a9ac9c381bda2
EluneyMo <morales.eluney.epet20@gmail.com>
2023-09-16 11:06:14 -0300

    Importando librerias

--- assets/src/home/Home.jsx
+++ assets/src/home/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { ReactDOM } from 'react'
 import appFirebase from '../../../firebase/firebase'
+import {useHistory} from "react-router-dom"
 import { getAuth, signOut } from 'firebase/auth'
 import { Link, BrowserRouter as Router } from "react-router-dom"
 import Button from "@material-ui/core/Button"
 import "../home/home.css"
+import Panicbutton from '../panico/Panicbutton'
 const auth=getAuth(appFirebase)
 const Home = ({correoUsuario}) => {
   return (
     <div>
       <h1>Bienvenido {correoUsuario}  <button className='btn btn-primary' onClick={()=>signOut(auth)}>Cerrar sesion</button></h1>
-    <Router path="/assets/src/panico/Panicbutton.jsx">
+    <Router path="/assets/src/panico/Panicbutton.jsx" component={Panicbutton}>
     <Link to="/BotonPanico/">
     <button className='boton' />
     </Link>
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { ReactDOM } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "./App";
+import Panicbutton from "./assets/src/panico/Panicbutton";
+import Home from "./assets/src/home/Home";
 function Main(){
     return(
-        <Router>
-            <App />
+        <Router> 
+            <Route path="/App.tsx" Component={App} />
+            <Route path="/assets/src/home" Component={Home}/>
+            <Route path="/assets/src/Panicbutton.jsx" Component={Panicbutton} />
         </Router>
     );
 }
