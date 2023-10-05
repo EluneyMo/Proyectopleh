72e73912ce70475638cb84202302c7a4b3f62d02
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-10-05 10:32:47 -0300

    agrego button home opara acceder al home

---
+++ assets/src/HomeButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+function iralhome() {
+  const navigation = useNavigation();
+  const iralhome = () => {
+    navigation.navigate("./Home");
+  };
+  return <Button title="home" onPress={iralhome} />;
+}
--- assets/src/Retrocederbutton.jsx
+++ assets/src/Retrocederbutton.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
 import { FontAwesomeIcon } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 function RetrocederButton() {
