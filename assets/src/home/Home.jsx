0723ae9b489f39aba540adf54f7dcc25d3fb9d50
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-10-05 09:17:31 -0300

    creo search con faltant de link para agregar y ser usada, tiene48 lineas de codigo

--- assets/src/home/Home.jsx
+++ assets/src/home/Home.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import appFirebase from '../../../firebase/firebase'
-import { getAuth, signOut } from 'firebase/auth'
-const auth=getAuth(appFirebase)
-const Home = ({correoUsuario}) => {
+import React from "react";
+import appFirebase from "../../../firebase/firebase";
+import { getAuth, signOut } from "firebase/auth";
+import x from "./../preguntas/Buttonpreguntas.jsx";
+import Buttonpreguntas from "./../preguntas/Buttonpreguntas.jsx";
+import x from "/proyectopleh/assets/searchpoleh/.searchone.jsx";
+const auth = getAuth(appFirebase);
+const Home = ({ correoUsuario }) => {
   return (
     <div>
-      <h1>Bienvenido {correoUsuario}  <button className='btn btn-primary' onClick={()=>signOut(auth)}>Cerrar sesion</button></h1>
+      <h1>PLEH</h1>
+      <h1>
+        Bienvenido {correoUsuario}{" "}
+        <button className="btn btn-primary" onClick={() => signOut(auth)}>
+          Cerrar sesion
+        </button>
+      </h1>
+      <Buttonpreguntas></Buttonpreguntas>
     </div>
-  
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ assets/src/preguntas/Buttonpreguntas.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Buttonpreguntas = () => {
+  return (
+    <div>
+      <button>
+        <Link></Link>
+      </button>
+    </div>
+  );
+};
+
+export default Buttonpreguntas;
