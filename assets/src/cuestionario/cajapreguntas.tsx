83bdf58d4bcc6fdde0e7f80a9a23773fd19bd22e
nata <mercadonatali.epet20@gmail.com>
2023-10-10 11:56:48 -0300

    Creando div card

--- assets/src/cuestionario/cajapreguntas.tsx
+++ assets/src/cuestionario/cajapreguntas.tsx
@@ -102,6 +102,9 @@ function CajaTipos() {
       <div className="">
         <div className="card" style={{ width: '18rem'}}>
           <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+
+          </div>
         </div>
       </div>
     </>
