7edd40e55053708c4e84e25c736529411a9d8352
nata <mercadonatali.epet20@gmail.com>
2023-10-10 12:07:45 -0300

    Eliminando errores

--- assets/src/cuestionario/cajapreguntas.tsx
+++ assets/src/cuestionario/cajapreguntas.tsx
@@ -109,24 +109,12 @@ function CajaTipos() {
           </div>
         </div>
       </div>
+
+
     </>
   );
 }
 
-function Card() {
-  return (
-    <>
-      <h2>Noticias Recientes</h2>
-      <div className="card" style={{ width: '18rem' }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Violencia de Genero</h5>
-          <p className="card-text">La violencia de género puede ser de tipo sexual, físico, psicológico y económico, y puede darse tanto en el espacio público como en la esfera privada</p>
-          <a href="#" className="btn btn-primary">Ver más información</a>
-        </div>
-      </div>
-    </>
-  );
-}
+
 
 export default CajaTipos;
