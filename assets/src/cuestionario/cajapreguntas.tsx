5f0684c1c07d51f3f1b546f91c88833ffb345c75
nata <mercadonatali.epet20@gmail.com>
2023-10-10 12:28:43 -0300

    Creando carpeta tipos

--- assets/src/cuestionario/cajapreguntas.tsx
+++ assets/src/cuestionario/cajapreguntas.tsx
@@ -104,8 +104,10 @@ function CajaTipos() {
           <img src="..." className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">Una mujer denunció que un amigo la acompañó a su casa, la amenazó con un cuchillo y la violó</h5>
-            <p className="card-text">Una joven de 20 años denunció que un amigo, tras acompañarla a su casa en la localidad de Oberá, Misiones, la amenazó con un cuchillo, la violó repetidas veces y grabó el ataque con un celular, el domingo a la madrugada. Tras ello el hombre que fue detenido y su casa, allanada en busca de indicios</p>
-            <a href="#" className="btn btn-primary">Ver más información</a>
+            <p className="card-text">Una joven de 20 años denunció que un amigo, tras acompañarla a su casa en la 
+            localidad de Oberá, Misiones, la amenazó con un cuchillo, la violó repetidas veces y grabó el ataque con un 
+            celular, el domingo a la madrugada. Tras ello el hombre que fue detenido y su casa, allanada en busca de indicios</p>
+            <a href="https://www.lanacion.com.ar/seguridad/misiones-una-mujer-denuncio-que-un-amigo-la-acompano-a-su-casa-la-amenazo-con-un-cuchillo-y-la-violo-nid04102023/" className="btn btn-primary">Ver más información</a>
           </div>
         </div>
       </div>
