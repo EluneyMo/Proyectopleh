5944b0bddd8e3d2b69db367eab1751df4c1c3487
nata <mercadonatali.epet20@gmail.com>
2023-10-10 09:55:35 -0300

    Creando div

--- assets/src/cuestionario/cajapreguntas.jsx
+++ assets/src/cuestionario/cajapreguntas.jsx
@@ -5,18 +5,19 @@ import React from "react";
 
 function cajaTipos(){
     return(
-        
-        <i className="bi bi-chevron-left">#</i>,
-        <h2>Tipos de violencias</h2>,
-        <div className="card text-bg-dark">
-            <img src="..." className="card-img" alt="..." />
-            <div className="card-img-overlay">
-               <h5 className="card-title">Violencia Domestica</h5>
-               <p className="card-text">La violencia no es solo matar a otro</p> 
-            </div>
+        <div className="Tviolencias">
+            <i className="bi bi-chevron-left">#</i>,
+            <h2>Tipos de violencias</h2>,
+            <div className="card text-bg-dark">
+                <img src="..." className="card-img" alt="..." />
+                <div className="card-img-overlay">
+                <h5 className="card-title">Violencia Domestica</h5>
+                <p className="card-text">La violencia no es solo matar a otro</p> 
+                </div>
 
-        </div>,
-        <i className="bi bi-chevron-right">#</i>
+            </div>,
+            <i className="bi bi-chevron-right">#</i>
+        </div>
     )
 }
 
