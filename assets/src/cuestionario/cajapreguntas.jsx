b9abf26a4a24184ca4780052f3ba29b8b186019d
nata <mercadonatali.epet20@gmail.com>
2023-10-10 10:32:39 -0300

    Creando tarjeta imagen

--- assets/src/cuestionario/cajapreguntas.jsx
+++ assets/src/cuestionario/cajapreguntas.jsx
@@ -22,7 +22,10 @@ function cajaTipos(){
         <div className="VSexual">
             <div className="card text-bg-dark">
                <img src="..." className="imagen2" alt="..." /> 
-
+               <div className="card-img">
+                
+               </div>
+            
             </div>
 
 
