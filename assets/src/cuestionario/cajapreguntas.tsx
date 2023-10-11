7ecf810b4844c7bdbcdaf306d1039431c558d5d8
nata <mercadonatali.epet20@gmail.com>
2023-10-11 12:39:30 -0300

    Agregando cambios

--- assets/src/cuestionario/cajapreguntas.tsx
+++ assets/src/cuestionario/cajapreguntas.tsx
@@ -1,122 +1,147 @@
 import React from "react";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import {Card} from "react-native-elements" 
 
 function CajaTipos() {
   return (
-    <>
-      <div className="Vdomestica">
-        <i className="bi bi-chevron-left">#</i>
-        <h2>Tipos de violencias</h2>
-        <div className="card text-bg-dark">
-          <img src="..." className="card-img" alt="..." />
-          <div className="card-img-overlay">
-            <h5 className="card-title">Violencia Domestica</h5>
-            <p className="card-text">La violencia no es solo matar a otro</p>
-          </div>
-        </div>
-        <i className="bi bi-chevron-right">#</i>
-      </div>
+    <View>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+        <Text>#</Text>
+        <Text>Tipos de violencias</Text>
+        <Text>#</Text>
+      </View>
 
-      <div className="VSexual">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen2" alt="..." />
-          <div className="card-img">
-            <h5 className="title2">Violencia Sexual</h5>
-          </div>
-        </div>
-      </div>
+      <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ color: "#555", fontSize: 18 }}>Violencia Domestica</Text>
+          <Text style={{ color: "#999" }}>La violencia no es solo matar a otro</Text>
+          <Image 
+          source={require("")}
+          style={{ width: 200, height: 200 }}/>
+        </View>
+      </View>
 
-      <div className="VLaboral">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen3" alt="..." />
-          <div className=" card-img3">
-            <h5 className="title3">Violencia Laboral</h5>
-          </div>
-        </div>
-      </div>
+       <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ color: "#555", fontSize: 18 }}>Violencia Sexual </Text>
+          <Image 
+          source={require("")}
+          style={{ width: 200, height: 200 }}/>
+        </View>
+      </View>
 
-      <div className="Femicidio">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen4" alt="..." />
-          <div className="card-img4">
-            <h5 className="title4">Femicidio</h5>
-          </div>
-        </div>
-      </div>
+      <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Violencia Laboral</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
-      <div className="VInstitucional">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen5" alt="..." />
-          <div className="card-img5">
-            <h5 className="title5">Violencia institucional</h5>
-          </div>
-        </div>
-      </div>
+     <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Femicidio</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
-      <div className="VMediatica">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen6" alt="..." />
-          <div className="card-img6">
-            <h5 className="title6">Violencia Mediática</h5>
-          </div>
-        </div>
-      </div>
+     <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Violencia Institucional</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
-      <div className="Acoso">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen7" alt="..." />
-          <div className="card-img7">
-            <h5 className="title7">Acoso</h5>
-          </div>
-        </div>
-      </div>
+     <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Violencia Mediática</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
-      <div className="VPsicologica">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen8" alt="..." />
-          <div className="card-img8">
-            <h5 className="title8">Violencia Psicológica</h5>
-          </div>
-        </div>
-      </div>
+     <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Acoso</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
-      <div className="VEconomica">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen9" alt="..." />
-          <div className="card-img9">
-            <h5 className="title9">Violencia Economica y Patrimonial</h5>
-          </div>
-        </div>
-      </div>
+     <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Violencia Psicologica</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
-      <div className="VFisica">
-        <div className="card text-bg-dark">
-          <img src="..." className="imagen10" alt="..." />
-          <div className="card-img10">
-            <h5 className="title10">Violencia Física</h5>
-          </div>
-        </div>
-      </div> 
+     <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Violencia Economica y Patrimonial</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
-      <h2>Noticias Recientes</h2>
-
-      <div className="">
-        <div className="card" style={{ width: '18rem'}}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Una mujer denunció que un amigo la acompañó a su casa, la amenazó con un cuchillo y la violó</h5>
-            <p className="card-text">Una joven de 20 años denunció que un amigo, tras acompañarla a su casa en la 
-            localidad de Oberá, Misiones, la amenazó con un cuchillo, la violó repetidas veces y grabó el ataque con un 
-            celular, el domingo a la madrugada. Tras ello el hombre que fue detenido y su casa, allanada en busca de indicios</p>
-            <a href="https://www.lanacion.com.ar/seguridad/misiones-una-mujer-denuncio-que-un-amigo-la-acompano-a-su-casa-la-amenazo-con-un-cuchillo-y-la-violo-nid04102023/" className="btn btn-primary">Ver más información</a>
-          </div>
-        </div>
-      </div>
+     <View style={{ backgroundColor: "#fff", width:400,}}>
+       
+       <View style={{ alignItems: "center" }}>
+         <Text style={{ color: "#555", fontSize: 18 }}>Violencia Física</Text>
+         <Image 
+         source={require("")}
+         style={{ width: 200, height: 200 }}/>
+       </View>
+     </View>
 
 
-    </>
+
+      {/* Repite la estructura anterior para otros tipos de violencia */}
+
+      
+
+      
+      
+      <Text>Noticias Recientes</Text>
+
+
+      
+        <Image
+          source={require("")}
+          style={{ width: 200, height: 200 }}
+        />
+        <View>
+          <Text>Una mujer denunció que un amigo la acompañó a su casa, la amenazó con un cuchillo y la violó</Text>
+          <Text>
+            Una joven de 20 años denunció que un amigo, tras acompañarla a su casa en la
+            localidad de Oberá, Misiones, la amenazó con un cuchillo, la violó repetidas veces y grabó el ataque con un
+            celular, el domingo a la madrugada. Tras ello el hombre que fue detenido y su casa, allanada en busca de indicios
+          </Text>
+          <TouchableOpacity onPress={() => Linking.openURL("https://www.lanacion.com.ar/seguridad/misiones-una-mujer-denuncio-que-un-amigo-la-acompano-a-su-casa-la-amenazo-con-un-cuchillo-y-la-violo-nid04102023/")}>
+            <Text>Ver más información</Text>
+          </TouchableOpacity>
+        </View>
+      
+    </View>
   );
 }
-
-
 
 export default CajaTipos;
