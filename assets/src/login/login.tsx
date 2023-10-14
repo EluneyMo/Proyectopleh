5c5a0e5294f6bd3b3b2532dcca8343e51f3d1c6e
EluneyMo <morales.eluney.epet20@gmail.com>
2023-10-13 23:41:31 -0300

    Cambiando tamaño

--- assets/src/login/login.tsx
+++ assets/src/login/login.tsx
@@ -136,7 +136,7 @@ const styles = StyleSheet.create({
     textAlign:"center"
   },
   input: {
-    height: 40,
+    height: 30,
     borderColor: "gray",
     borderWidth: 1,
     marginBottom: 16,
