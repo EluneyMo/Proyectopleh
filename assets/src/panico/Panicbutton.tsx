b25047e1e64698551614274c9dfef4772f700293
EluneyMo <morales.eluney.epet20@gmail.com>
2023-10-14 14:36:14 -0300

    Agregando estilos

--- assets/src/panico/Panicbutton.tsx
+++ assets/src/panico/Panicbutton.tsx
@@ -18,7 +18,7 @@ const Panicbutton: React.FC <SoundButtonProps> = ({ soundUri }) => {
     <View style={styles.container}>
       <Text style={styles.imp}>¡IMPORTANTE!</Text>
       <Text style={styles.pre}>Presione el boton rojo de panico si esta en peligro</Text>
-      <Text>Al accionar el boton de panico se lanzara una alarma a las autoridades y contactos de confianza. 
+      <Text style={styles.te}>Al accionar el boton de panico se lanzara una alarma a las autoridades y contactos de confianza. 
         MUCHO CUIDADO, el accionarla solo para bromas podria ocasionar problemas legales.</Text>
       <TouchableOpacity onPress={playAlarmSound}>
       <Image 
@@ -50,6 +50,9 @@ const styles = StyleSheet.create({
     fontSize:20,
     textAlign:"center"
   },
+  te:{
+    
+  }
 });
 
 export default Panicbutton;
