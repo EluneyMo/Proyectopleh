05bd3f19859dda991981479b08056716b6ce17a3
EluneyMo <morales.eluney.epet20@gmail.com>
2023-10-14 12:28:40 -0300

    Agregando texto

--- assets/src/panico/Panicbutton.tsx
+++ assets/src/panico/Panicbutton.tsx
@@ -6,6 +6,8 @@ const Panicbutton: React.FC = () => {
     <View style={styles.container}>
       <Text>¡IMPORTANTE!</Text>
       <Text>Presione el boton rojo de panico si esta en peligro</Text>
+      <Text>Al accionar el boton de panico se lanzara una alarma a las autoridades y contactos de confianza. 
+        MUCHO CUIDADO, el accionarla solo para bromas podria ocasionar problemas legales.</Text>
     </View>
   );
 };
