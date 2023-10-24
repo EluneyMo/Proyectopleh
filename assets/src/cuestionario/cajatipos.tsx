4e24e8a4d7eb913a9fea6602d5a4b651f6a61f39
nata <mercadonatali.epet20@gmail.com>
2023-10-24 18:50:20 -0300

    Agregando imagen

--- assets/src/cuestionario/cajatipos.tsx
+++ assets/src/cuestionario/cajatipos.tsx
@@ -5,6 +5,7 @@ function Tipos() {
   return (
     <View>
       <Text>Violencia de Género</Text>
+      <Image source={require('../tipos/violenciagenero.jpeg')} style={{ width: 100, height: 100 }} />
       
     </View>
   );
