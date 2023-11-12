ea0bdc8c40ba6d2938a39bdf5e84b35b59dc2e43
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 04:57:05 -0300

    agregando hover

--- assets/src/expertos/botton_selc.jsx
+++ assets/src/expertos/botton_selc.jsx
@@ -41,6 +41,9 @@ const BootstrapButton = styled(Button)({
   const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(purple[500]),
     backgroundColor: purple[500],
+    '&:hover': {
+        
+      },
   }));
   
     
