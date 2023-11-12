6db61cf0602ab23032d74b06a625f996ed831434
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 04:35:55 -0300

    importando el color

--- assets/src/expertos/botton_selc.jsx
+++ assets/src/expertos/botton_selc.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import { purple } from '@mui/material/colors';
   return (
 
     
