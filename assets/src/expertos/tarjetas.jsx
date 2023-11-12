32ed451df572b9f1c7b43c26ca4d5f7fc51e792d
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 03:06:39 -0300

    importando avatar

--- assets/src/expertos/tarjetas.jsx
+++ assets/src/expertos/tarjetas.jsx
@@ -5,6 +5,7 @@ import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar'; 
   return (
 
   );
