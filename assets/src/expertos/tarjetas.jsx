f733145010ea5758032f6bdf0ca0f88ab5ebfb1d
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 03:05:27 -0300

    importando el card action

--- assets/src/expertos/tarjetas.jsx
+++ assets/src/expertos/tarjetas.jsx
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
   return (
 
   );
