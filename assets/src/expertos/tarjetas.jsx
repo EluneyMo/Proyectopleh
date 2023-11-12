443f512dce3bfe2d6e9477a26c39d71b33951af1
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 03:04:23 -0300

    importando el header de la card

--- assets/src/expertos/tarjetas.jsx
+++ assets/src/expertos/tarjetas.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-
+import CardHeader from '@mui/material/CardHeader';
 
   return (
 
