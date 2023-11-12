adfe83fce2b23cc0d1cf6c1ba34eeb63ee0962fc
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 03:07:24 -0300

    importando incon button

--- assets/src/expertos/tarjetas.jsx
+++ assets/src/expertos/tarjetas.jsx
@@ -6,6 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar'; 
+import IconButton from '@mui/material/IconButton';
   return (
 
   );
