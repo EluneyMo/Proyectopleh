81b1c3ec2d9ef8b4888f00fe82f9d67186e19f15
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 03:06:05 -0300

    importando collapse

--- assets/src/expertos/tarjetas.jsx
+++ assets/src/expertos/tarjetas.jsx
@@ -4,6 +4,7 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
   return (
 
   );
