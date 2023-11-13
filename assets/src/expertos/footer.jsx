4d4a66aceed530d5627d48d611e44217e8785e18
abril005 <abril.levillan.epet20@gmail.com>
2023-11-13 00:37:16 -0300

    agregandole un color de fondo

--- assets/src/expertos/footer.jsx
+++ assets/src/expertos/footer.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
     
     const FooterContainer = styled('footer')({
-        
+        backgroundColor: 'violet',
     });
 
     return (
