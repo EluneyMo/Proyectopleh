96aaa61d5abe106ba037f0dc5c1d6b3ac283d42e
abril005 <abril.levillan.epet20@gmail.com>
2023-11-13 00:37:31 -0300

    agregandole un padding

--- assets/src/expertos/footer.jsx
+++ assets/src/expertos/footer.jsx
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
     
     const FooterContainer = styled('footer')({
         backgroundColor: 'violet',
+        padding: '20px',
     });
 
     return (
