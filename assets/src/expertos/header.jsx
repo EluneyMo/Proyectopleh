9732853a56352ebbfb4cbe144427b88f3edda07c
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 22:28:26 -0300

    importando el icono del menu

--- assets/src/expertos/header.jsx
+++ assets/src/expertos/header.jsx
@@ -5,6 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function AppBar() {
   return (
