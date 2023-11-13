356ba2f7b7524e588f98343dfdbd73285e34c42a
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 22:00:48 -0300

    agregado otro caso

--- assets/src/expertos/datos.js
+++ assets/src/expertos/datos.js
@@ -8,4 +8,14 @@ const datosFalsos = [
         categoria: 'psicologo',
         additionalInfo: 'Trabaja de lunes a viernes de 8am a 3pm solo acepta pacientes con turno. ',
     },
+
+    {
+        id: 1,
+        fullName: 'Comisaria N°1',
+        title: 'Comisaria',
+        location: 'Neuquen, Neuquén ',
+        phoneNumber: '+5429922567',
+        categoria: 'Comisaria',
+        additionalInfo: 'Trabaja de lunes a viernes de 8am a 3pm ',
+    },
   ];
