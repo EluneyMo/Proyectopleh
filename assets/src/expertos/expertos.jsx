e3a3164d14edc6bb156cdce4adf46a04aa88c43f
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 21:11:09 -0300

    agregando constate para guardar datos falsos

--- assets/src/expertos/expertos.jsx
+++ assets/src/expertos/expertos.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Tarjeta from './tarjeta';
 const Expertos = () => {
     const [expertosData, setExpertosData] = useState([]);
+    const datosFalsos = [
+        
+      ];
     };
+
+
 function ExpertosPage() {
   return (
         <div>
