fcd9df4d859d187bc38aeef99f2d7c64c7306e15
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 21:38:10 -0300

    agregado categoria la caso falso

--- assets/src/expertos/expertos.jsx
+++ assets/src/expertos/expertos.jsx
@@ -11,6 +11,7 @@ const Expertos = () => {
             title: 'Psicólogo',
             location: 'Neuquen, Neuquén ',
             phoneNumber: '+5429922567',
+            categoria: 'psicologo',
             additionalInfo: 'Trabaja de lunes a viernes de 8am a 3pm solo acepta pacientes con turno. ',
         },
       ];
