24feea8cef629c3775119281bfef8fb077b1ba67
abril005 <abril.levillan.epet20@gmail.com>
2023-11-13 00:39:18 -0300

    llando a  FooterContainer

--- assets/src/expertos/footer.jsx
+++ assets/src/expertos/footer.jsx
@@ -8,7 +8,9 @@ import { styled } from '@mui/material/styles';
 
     const Footer = () => {
         return (
-    
+            <FooterContainer>
+                
+            </FooterContainer>
         );
     };
 
