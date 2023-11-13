10ba0bb5d4f987d53dac1dc91c18ce6d14b92071
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 21:51:19 -0300

    agregando  const handleButtonClick

--- assets/src/expertos/botton_selc.jsx
+++ assets/src/expertos/botton_selc.jsx
@@ -49,7 +49,8 @@ const ColorButton = styled(Button)(({ theme }) => ({
 }));
 
     const CustomizedButtons = ({ onSelect }) =>{
-    
+        const handleButtonClick {
+        };
     };
 
   return (
