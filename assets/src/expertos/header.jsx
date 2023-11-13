f522c42c3603a1056d509941f54ee5a555364c4a
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 22:27:32 -0300

    importando la typography

--- assets/src/expertos/header.jsx
+++ assets/src/expertos/header.jsx
@@ -3,6 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 
 function AppBar() {
   return (
