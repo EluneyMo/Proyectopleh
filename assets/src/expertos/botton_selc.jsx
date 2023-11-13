9cf243327284faaf162c084930f7386759e1cc46
abril005 <abril.levillan.epet20@gmail.com>
2023-11-13 00:28:35 -0300

    dandole un degrade de color violeta al boton

--- assets/src/expertos/botton_selc.jsx
+++ assets/src/expertos/botton_selc.jsx
@@ -4,67 +4,55 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { purple } from '@mui/material/colors';
 
-const BootstrapButton = styled(Button)({
-  boxShadow: 'none',
-  textTransform: 'none',
-  fontSize: 16,
-  padding: '6px 12px',
-  border: '1px solid',
-  lineHeight: 1.5,
-  backgroundColor: '#0063cc',
-  borderColor: '#0063cc',
-  fontFamily: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    '"Segoe UI"',
-    'Roboto',
-    '"Helvetica Neue"',
-    'Arial',
-    'sans-serif',
-    '"Apple Color Emoji"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-  ].join(','),
-  '&:hover': {
-    backgroundColor: '#0069d9',
-    borderColor: '#0062cc',
+const GradientButton = styled(Button)(({ theme }) => ({
+    background: `linear-gradient(to right, ${theme.palette.purple[100]}, ${theme.palette.purple[700]})`,
+    border: '1px solid',
     boxShadow: 'none',
-  },
-  '&:active': {
-    boxShadow: 'none',
-    backgroundColor: '#0062cc',
-    borderColor: '#005cbf',
-  },
-  '&:focus': {
-    boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-  },
-});
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    lineHeight: 1.5,
+    fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+        background: `linear-gradient(to right, ${theme.palette.purple[200]}, ${theme.palette.purple[800]})`,
+        boxShadow: 'none',
+    },
+    '&:active': {
+        boxShadow: 'none',
+        background: `linear-gradient(to right, ${theme.palette.purple[300]}, ${theme.palette.purple[900]})`,
+    },
+    '&:focus': {
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+    }));
 
-const ColorButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText(purple[500]),
-  backgroundColor: purple[500],
-  '&:hover': {
-    backgroundColor: purple[700],
-  },
-}));
+    const CustomizedButtons = ({ onSelect }) => {
+    const handleButtonClick = (categoria) => {
+        onSelect(categoria);
+    };
 
-    const CustomizedButtons = ({ onSelect }) =>{
-        const handleButtonClick  = (categoria) =>  {
-            onSelect(categoria);
-        };
+    return (
+        <Stack spacing={5} direction="row">
+            <GradientButton variant="contained" onClick={() => handleButtonClick('psicologos')}>
+                Psicólogos
+            </GradientButton>
 
-  return (
-    <Stack spacing={5} direction="row">
-        <Button variant="contained" ck={() => handleButtonClick('psicologos')}>
-        Psicólogos
-        </Button >
-
-        <Button variant="contained" onClick={() => handleButtonClick('comisarias')}>
-        Comisarías
-       </Button>
-
-    </Stack>
-  );
+            <GradientButton variant="contained" onClick={() => handleButtonClick('comisarias')}>
+                    Comisarías
+            </GradientButton>
+        </Stack>
+    );
 };
 
 export default CustomizedButtons;
