6fcd2d9ec771fd03f9b772a1df9d890fe50b2cb8
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 21:16:59 -0300

    agregando additionalInfo

--- assets/src/expertos/expertos.jsx
+++ assets/src/expertos/expertos.jsx
@@ -9,6 +9,7 @@ const Expertos = () => {
             title: 'Psicólogo',
             location: 'Neuquen, Neuquén ',
             phoneNumber: '+5429922567',
+            additionalInfo: 'Trabaja de lunes a viernes de 8am a 3pm solo acepta pacientes con turno. ',
         },
       ];
     };
