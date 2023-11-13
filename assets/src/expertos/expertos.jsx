ee9942d28fd733baba1bdda3a1481884720c5a7f
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 21:59:06 -0300

    borrando los datos falsos de exoertos

--- assets/src/expertos/expertos.jsx
+++ assets/src/expertos/expertos.jsx
@@ -6,17 +6,7 @@ const Expertos = () => {
     
     const [expertosData, setExpertosData] = useState([]);
     const [filtro, setFiltro] = useState(null);
-    const datosFalsos = [
-        {
-            id: 1,
-            fullName: 'Carla Lopez',
-            title: 'Psicólogo',
-            location: 'Neuquen, Neuquén ',
-            phoneNumber: '+5429922567',
-            categoria: 'psicologo',
-            additionalInfo: 'Trabaja de lunes a viernes de 8am a 3pm solo acepta pacientes con turno. ',
-        },
-      ];
+   
       setExpertosData(datosFalsos);
     };
 
