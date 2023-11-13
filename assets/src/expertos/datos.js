2a5c765259e41722c448758d0725bdab6f16ebaf
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 21:59:35 -0300

    guardando los datos falsos en el archivo datos.js

---
+++ assets/src/expertos/datos.js
@@ -0,0 +1,11 @@
+const datosFalsos = [
+    {
+        id: 1,
+        fullName: 'Carla Lopez',
+        title: 'Psicólogo',
+        location: 'Neuquen, Neuquén ',
+        phoneNumber: '+5429922567',
+        categoria: 'psicologo',
+        additionalInfo: 'Trabaja de lunes a viernes de 8am a 3pm solo acepta pacientes con turno. ',
+    },
+  ];
