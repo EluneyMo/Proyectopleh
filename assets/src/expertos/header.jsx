6537f6d8ab52f15740fde644c2117cdf3a67f269
abril005 <abril.levillan.epet20@gmail.com>
2023-11-12 22:33:50 -0300

    creando la contante settings

--- assets/src/expertos/header.jsx
+++ assets/src/expertos/header.jsx
@@ -14,6 +14,7 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['Products', 'Pricing','Blog'];
+const settings = [];
 
 function AppBar() {
   return (
