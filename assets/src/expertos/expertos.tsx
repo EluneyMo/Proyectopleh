c8215b706cdf0c2eff420e2b4c69a28227c4d64a
abril005 <abril.levillan.epet20@gmail.com>
2023-11-13 01:31:56 -0300

    cambiando los nombres de los componentes

--- assets/src/expertos/expertos.tsx
+++ assets/src/expertos/expertos.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import Tarjeta from './tarjeta';
+import ProfileCard from './tarjeta';
 import CustomizedButtons from './botton_selc';
 import { datosFalsos } from './datos';
-import AppBar from './header.jsx'; // Reemplazar con la ruta correcta a tu componente AppBar
+import CustomAppBar from './header'; 
 import Footer from './footer';
 
 const Expertos: React.FC = () => {
@@ -18,11 +18,11 @@ const Expertos: React.FC = () => {
 
     return (
         <div>
-            <AppBar />
+            <CustomAppBar />
             <CustomizedButtons onSelect={(categoria) => setFiltro(categoria)} />
             <h1>Lista de Expertos</h1>
             {expertosFiltrados.map((experto) => (
-                <Tarjeta key={experto.id} data={experto} />
+                <ProfileCard key={experto.id} data={experto} />
             ))}
             <Footer />
         </div>
