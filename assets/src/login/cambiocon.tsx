4a0da9d9b520ccfe92e47426ed3238540f05a63e
EluneyMo <morales.eluney.epet20@gmail.com>
2023-11-14 11:24:31 -0300

    Eliminando

--- assets/src/login/cambiocon.tsx
+++ assets/src/login/cambiocon.tsx
@@ -3,13 +3,9 @@ import { View, Text, TextInput, Button, StyleSheet, Alert, Image, TouchableOpaci
 import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signInAnonymously, updatePassword, EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
 import "firebase/auth"
 import {LinearGradient} from "expo-linear-gradient"
-import * as Google from "expo-google-app-auth"
 import appFirebase from "../../../firebase/firebase";
 import { useNavigation } from '@react-navigation/native';
 import { FirebaseError } from "firebase/app";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getFirestore, collection, doc, setDoc,DocumentReference, DocumentData  } from 'firebase/firestore';
-import firebase from "firebase/app"
 import "firebase/auth"
 import Toast from "react-native-toast-message";
 const auth = getAuth(appFirebase);
