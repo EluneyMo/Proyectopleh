5f50fc3d130f4532f353a70322814481ec2121dd
EluneyMo <morales.eluney.epet20@gmail.com>
2023-11-14 11:22:15 -0300

    Importando librerias

--- assets/src/login/gmailcontra.tsx
+++ assets/src/login/gmailcontra.tsx
@@ -1,2 +1,4 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { Auth } from 'firebase/auth';
+import appFirebase from '../../../firebase/firebase';
