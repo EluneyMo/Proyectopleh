2280e304fbf50e09c26f0d4edcf89c3d61df532c
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-11-14 11:15:09 -0300

    agrego constantes para users y para search con states

--- assets/src/searchpleh/searchone.jsx
+++ assets/src/searchpleh/searchone.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 const searchone = () => {
+  const { users, setUsers } = useState({});
+  const { search, useSearch } = useState("");
+
     return (
     
   )
