da9c35a63b165063e5ad25380f671fff2aea8368
AlenSalazar <alen.salazar.epet20@gmail.com>
2023-11-14 22:57:07 -0300

    cambiando a direccion

--- assets/src/usuario/usuario.tsx
+++ assets/src/usuario/usuario.tsx
@@ -45,6 +45,7 @@ const MyForm: React.FC<MyFormProps> = () => {
             <TextInput
             placeholder="imputDireccion"
             value={imputDireccion}
+            onChangeText={(Text) => setDireccion(Text)}
             />
         </View>
     )
