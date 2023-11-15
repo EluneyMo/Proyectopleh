e4815da4a7ae5ebf7ee3ad5d5f396efc492acbf4
AlenSalazar <alen.salazar.epet20@gmail.com>
2023-11-14 22:12:40 -0300

    definiendo numIdentificacion

--- assets/src/usuario/usuario.tsx
+++ assets/src/usuario/usuario.tsx
@@ -6,4 +6,5 @@ interface MyFormProps {}
 const MyForm: React.FC<MyFormProps> = () => {
     const [imputNombre, setNombre]  = useState<string>('');
     const [imputSurname, setSurname] = useState<string>('');
+    const [imputNumeroIdentificacion, setNumeroIdentificacion] = useState<string>('');
 }
