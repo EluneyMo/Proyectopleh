9911c3f37ac5b037b2f04c437becfb5d484ae6cd
nata <mercadonatali.epet20@gmail.com>
2023-11-16 16:09:57 -0300

    Creando  titulo

--- assets/src/cuestionario/mediatica.tsx
+++ assets/src/cuestionario/mediatica.tsx
@@ -5,7 +5,7 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 function Mediática() {
     return(
         <View>
-            
+            <Text>Violencia Mediatica</Text>
 
         </View>
     )
