c60191789770530afd528ccc3e7ccf8c83c472fd
nata <mercadonatali.epet20@gmail.com>
2023-11-16 16:50:54 -0300

    Borrando errores

--- assets/src/cuestionario/cajapreguntas.tsx
+++ assets/src/cuestionario/cajapreguntas.tsx
@@ -77,24 +77,7 @@ const CajaTipos:React.FC<PreguntasProps>=({route,navigation}) => {
               </TouchableOpacity>    
               
               
-              <TouchableOpacity onPress={movete} style={styles.card}>
-                <Text style={styles.cardText}>Violencia Psicologica</Text>
-                <Image source={require("../tipos/psicologica.jpeg")} style={styles.cardimagen} />
-              </TouchableOpacity>
-              
-              <TouchableOpacity onPress={movete} style={styles.card}>
-                <Text style={styles.cardText}>Violencia Economica y Patrimonial</Text>
-                <Image source={require("../tipos/economica.jpeg")} style={styles.cardimagen} />
-              </TouchableOpacity>      
-              
-              <TouchableOpacity onPress={movete} style={styles.card}>
-                <Text style={styles.cardText}>Violencia Física</Text>
-                <Image source={require("../tipos/fisica.jpg")} style={styles.cardimagen} />
-              </TouchableOpacity>  
-            </View>   
-        
-
-          
+                   
           
           
 
