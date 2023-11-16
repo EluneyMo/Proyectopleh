7917c0f811ede857abea7c00b333bb4a1389d31a
nata <mercadonatali.epet20@gmail.com>
2023-11-16 12:36:28 -0300

    Creando titulo

--- assets/src/cuestionario/institucional.tsx
+++ assets/src/cuestionario/institucional.tsx
@@ -4,7 +4,7 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 function Institucional(){
     return(
         <View>
-            
+            <Text>Violencia Institucional</Text>
         </View>
     )
 }
