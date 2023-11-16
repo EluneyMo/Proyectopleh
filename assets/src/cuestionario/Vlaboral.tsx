1a313b65638528e4d388a8f017a3cefaacc11b4c
nata <mercadonatali.epet20@gmail.com>
2023-11-16 11:48:54 -0300

    Agregando texto

--- assets/src/cuestionario/Vlaboral.tsx
+++ assets/src/cuestionario/Vlaboral.tsx
@@ -7,6 +7,12 @@ function Laboral() {
         <View >
             <Text>Violencia Laboral</Text>
             <Image source={require('../tipos/laboral.jpeg')}></Image>
+            <Text>
+            La violencia en el entorno laboral abarca una amplia gama de comportamientos perjudiciales,
+            desde acoso verbal y psicológico hasta agresiones físicas. Puede ocurrir entre colegas,
+            entre empleados y empleadores, o incluso entre clientes y empleados. 
+            La clave para combatirla es identificarla a tiempo.
+            </Text>
         </View>
     )
 }
