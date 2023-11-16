bb76f0e4b4599e2a5d0dacda5ad2cf3900638ba6
EluneyMo <morales.eluney.epet20@gmail.com>
2023-11-16 09:48:20 -0300

    Agregando funcion para contraseña

--- assets/src/login/gmailcontra.tsx
+++ assets/src/login/gmailcontra.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
-import { Auth, getAuth } from 'firebase/auth';
+import { Auth, getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import appFirebase from '../../../firebase/firebase';
 const auth=getAuth(appFirebase)
 
 const enviargmai=()=>{
-
+    const handleResetPassword = async (email: string) => {
+        try {
+          await sendPasswordResetEmail(auth, email);
+          console.log("Se ha enviado un correo electrónico de restablecimiento de contraseña.");
+        } catch (error) {
+          console.error("Error al enviar el correo electrónico de restablecimiento de contraseña:", error);
+        }
+      };
 }
 export default enviargmai
