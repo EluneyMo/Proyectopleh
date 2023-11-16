d0792fe73577c5f182d8092dba486b5e504477d2
EluneyMo <morales.eluney.epet20@gmail.com>
2023-11-16 09:29:28 -0300

    Agregando funcio

--- assets/src/login/gmailcontra.tsx
+++ assets/src/login/gmailcontra.tsx
@@ -3,3 +3,7 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 import { Auth, getAuth } from 'firebase/auth';
 import appFirebase from '../../../firebase/firebase';
 const auth=getAuth(appFirebase)
+
+const enviargmai=()=>{
+    
+}
