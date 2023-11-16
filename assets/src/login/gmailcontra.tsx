445a70e04350b17d74deff61b46200f6b965d052
EluneyMo <morales.eluney.epet20@gmail.com>
2023-11-16 12:20:21 -0300

    Agregando navegacion

--- assets/src/login/gmailcontra.tsx
+++ assets/src/login/gmailcontra.tsx
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, ToastAndroid  } from 'react-native'
 import { Auth, getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import "firebase/functions";
+import { useNavigation } from '@react-navigation/native';
 import appFirebase from '../../../firebase/firebase';
 
 const auth=getAuth(appFirebase)
 
 const enviargmai=()=>{
+  const navigation = useNavigation();
+
     const [email, setEmail] = useState('');
     const handleResetPassword = async (email: string) => {
         try {
           await sendPasswordResetEmail(auth, email);
           console.log("Se ha enviado un correo electrónico de restablecimiento de contraseña.");
           alert("Se ha enviado un correo electrónico de restablecimiento de contraseña.");
+          navigation.navigate("Login" as never)
+
         } catch (error) {
           console.error("Error al enviar el correo electrónico de restablecimiento de contraseña:", error);
           alert ("Error al enviar el correo electrónico de restablecimiento de contraseña")
