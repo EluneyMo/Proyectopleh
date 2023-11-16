68e45d72e0c07118aa76134570bef70c42032897
nata <mercadonatali.epet20@gmail.com>
2023-11-16 10:47:50 -0300

    Contenedor padre

--- assets/src/cuestionario/vsexual.tsx
+++ assets/src/cuestionario/vsexual.tsx
@@ -3,6 +3,9 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 
 function Sexual{
     return(
-        
-    )
+        <View  style={styles.container}>
+            
+        </View>
+
+    );
 }
