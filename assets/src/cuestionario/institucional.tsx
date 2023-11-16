aa0fc3a7278037500b1294e95ff1450d22912527
nata <mercadonatali.epet20@gmail.com>
2023-11-16 12:45:19 -0300

    Agregando texto

--- assets/src/cuestionario/institucional.tsx
+++ assets/src/cuestionario/institucional.tsx
@@ -6,6 +6,19 @@ function Institucional(){
         <View>
             <Text>Violencia Institucional</Text>
             <Image source={require('../tipos/femicidios.jpg')}/>
+            <Text>
+                A veces, la violencia institucional puede ser sutil y pasar desapercibida, pero es importante estar 
+                consciente de sus manifestaciones para poder abordarlas de manera efectiva. 
+                Esta forma de violencia puede incluir acciones indebidas por 
+                parte de las instituciones gubernamentales, como la policía o el sistema judicial. La discriminación, 
+                el uso excesivo de la fuerza, la detención injustificada o el trato 
+                desigual son solo algunas de las expresiones de la violencia institucional. 
+                A menudo, las personas pueden no darse cuenta de que están siendo víctimas de este 
+                tipo de violencia, ya que las prácticas injustas pueden estar arraigadas 
+                en sistemas más amplios. Es crucial estar atentos/as a las señales, buscar 
+                información y, si es necesario, buscar apoyo para poner fin a estas situaciones 
+                y promover un cambio hacia sistemas más justos y equitativos.
+            </Text>
         </View>
     )
 }
